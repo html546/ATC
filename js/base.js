d82f1c3559cef36fe3aea412d27dc5b669692ee5
html546 <1776787598@qq.com>
2018-07-31 13:40:35 +0800

    继续修改bug

--- js/base.js
+++ js/base.js
@@ -80,3 +80,14 @@ function timestampToTime(timestamp) {
     s = date.getSeconds()<10? '0'+date.getSeconds() : date.getSeconds();
     return Y+M+D+h+m+s;}
 }
+
+function timestampToTime2(timestamp) {
+	if(!timestamp){
+		return ' ' 
+	}else {
+    var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = date.getDate()<10? '0'+date.getDate() + ' ':date.getDate() + ' ' ;
+    return Y+M+D;}
+}
